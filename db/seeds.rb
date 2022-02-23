e8e660b49b6f304266b6cb03b87505aef093ac98
Kazuya-Sakashita <kz0508@gmail.com>
2022-02-23 14:18:20 +0900

    add-navi

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+100.times do |p|
+   Pet.create!(
+      name: "ポチ#{p + 1}",
+      age: rand(1..3),
+      castration: rand(1..3),
+      category: rand(1..3),
+      character: rand(1..2),
+      gender: rand(1..4),
+      avairable_area: rand(1..47),
+      animal_type: rand(1..12),
+      description: "テストNo.#{p + 1}",
+   )
+end
